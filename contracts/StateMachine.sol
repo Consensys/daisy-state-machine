4439f1ce3e6315876547cb0c2b844c56d75ba6cc
Alice Henshaw <henshawalice@gmail.com>
2018-04-06 11:24:11 -0400

    Transition-oriented structure for the state machine allowing non-linear

--- contracts/StateMachine.sol
+++ contracts/StateMachine.sol
@@ -3,16 +3,32 @@ pragma solidity 0.4.19;
 
 contract StateMachine {
 
-    mapping(bytes32 => bytes32) public nextStateId;
-    // The identifiers for the available functions in each state
-    mapping(bytes32 => mapping(bytes4 => bool)) public allowedFunctions;
+    struct Transition {
+        // Ids of the transition's start and end states
+        bytes32 startState;
+        bytes32 endState;
 
-    mapping(bytes32 => function() internal[]) transitionCallbacks;
-    mapping(bytes32 => function(bytes32) internal returns(bool)[]) startConditions;
+        // a function that must be performed to transition into the new state
+        function() internal transitionEffect;
+
+        // condition which must be true to transition
+        function(bytes32) internal returns(bool) startCondition;
+    }
 
     // The current state id
     bytes32 public currentStateId;
 
+    // Specifies whether the state machine has been initialised
+    bool public hasBeenInitialised;
+
+    // Checks if a state id is valid
+    mapping(bytes32 => bool) public validStates;
+
+    // Maps state ids to their State structs
+    mapping(bytes32 => Transition[]) internal outgoingTransitions;
+
+    // stores allowed functions for each state
+    mapping(bytes32 => mapping(bytes4 => bool)) public allowedFunctions;
 
     event LogTransition(bytes32 stateId, uint256 blockNumber);
 
@@ -25,17 +41,34 @@ contract StateMachine {
         _;
     }
 
-    /// @dev Creates the given states.
-    /// @param stateIds Array of state ids.
-    function setStates(bytes32[] stateIds) internal {
-        require(stateIds.length > 0);
-        require(currentStateId == 0);
+    modifier checkInitialised {
+        require(hasBeenInitialised);
+        _;
+    }
 
-        currentStateId = stateIds[0];
-
-        for (uint256 i = 1; i < stateIds.length; i++) {
-            nextStateId[stateIds[i - 1]] = stateIds[i];
+    /// @dev creates the whole state machine from a start state and list of transitions
+    /// @dev the transitions will be parsed one by one each can only be parsed if the 
+    /// @dev start state is the initial state or has been the end state of an earlier transition
+    ///@param _initialState The initial state of the state machine
+    ///@param _transitions a list of transitions through the state machine
+    function setupStateMachine(bytes32 _initialState, Transition[] storage _transitions) internal {
+        require(!hasBeenInitialised);
+        currentStateId = _initialState;
+        validStates[_initialState] = true;
+        for (uint256 i = 0; i < _transitions.length; i++) {
+            createTransition(_transitions[i]);
         }
+        hasBeenInitialised = true;
+    }
+
+    /// @dev Creates a transition in the state machine
+    /// @param _fromId The id of the state from which the transition begins.
+    /// @param _toId The id of the state that will be reachable from "fromId".
+    function createTransition(Transition storage _transition) internal {
+        bytes32 startState = _transition.startState;
+        require(validStates[startState]);
+        validStates[_transition.endState] = true;
+        outgoingTransitions[startState].push(_transition);
     }
 
     /// @dev Allow a function in the given state.
@@ -45,53 +78,50 @@ contract StateMachine {
         allowedFunctions[stateId][functionSelector] = true;
     }
 
-    /// @dev Goes to the next state if posible (if the next state is valid)
-    function goToNextState() internal {
-        bytes32 next = nextStateId[currentStateId];
-        require(next != 0);
+    /// @dev Goes to the next state if possible (if the next state is valid)
+    /// @param _nextStateId stateId of the state to transition to
+    function goToNextState(bytes32 _nextStateId) public {
+        require(validStates[_nextStateId]);
 
-        currentStateId = next;
+        for (uint256 i = 0; i < outgoingTransitions[currentStateId].length; i++) {
+            if (outgoingTransitions[currentStateId][i].endState == _nextStateId) {
+                outgoingTransitions[currentStateId][i].transitionEffect();
+                currentStateId = _nextStateId;
+                LogTransition(_nextStateId, block.number);
 
-        for (uint256 i = 0; i < transitionCallbacks[next].length; i++) {
-            transitionCallbacks[next][i]();
+                break;
+            }
         }
-
-        LogTransition(next, block.number);
+             
     }
 
-    ///@dev add a function returning a boolean as a start condition for a state
-    ///@param stateId The ID of the state to add the condition for
-    ///@param condition Start condition function - returns true if a start condition (for a given state ID) is met
-    function addStartCondition(bytes32 stateId, function(bytes32) internal returns(bool) condition) internal {
-        startConditions[stateId].push(condition);
-    }
-
-    ///@dev add a callback function for a state
-    ///@param stateId The ID of the state to add a callback function for
-    ///@param callback The callback function to add (if the state is valid)
-    function addCallback(bytes32 stateId, function() internal callback) internal {
-        transitionCallbacks[stateId].push(callback);
-    }
 
     ///@dev transitions the state machine into the state it should currently be in
     ///@dev by taking into account the current conditions and how many further transitions can occur 
-    function conditionalTransitions() public {
+    function conditionalTransitions() internal {
 
-        bytes32 next = nextStateId[currentStateId];
+        Transition[] storage outgoing = outgoingTransitions[currentStateId];
 
-        while (next != 0) {
-            // If one of the next state's conditions is met, go to this state and continue
+        while (outgoing.length > 0) {
             bool stateChanged = false;
-            for (uint256 i = 0; i < startConditions[next].length; i++) {
-                if (startConditions[next][i](next)) {
-                    goToNextState();
-                    next = nextStateId[next];
+            //consider each of the next states in turn
+            for (uint256 j = 0; j < outgoing.length; j++) {
+                //Get the state that you are now to consider
+                Transition storage transition = outgoing[j];
+                // If this state's start condition is met, go to this state and continue
+                if (transition.startCondition(transition.endState)) {
+                    transition.transitionEffect();
+                    currentStateId = transition.endState;
+                    LogTransition(transition.endState, block.number);
+
+                    outgoing = outgoingTransitions[transition.endState];
                     stateChanged = true;
                     break;
                 }
             }
-            // If none of the next state's conditions are met, then we are in the right current state
+            //If we've tried all the possible following states and not changed, we're in the right state now
             if (!stateChanged) break;
         }
     }
+
 }
--- contracts/TimedStateMachine.sol
+++ contracts/TimedStateMachine.sol
@@ -6,41 +6,43 @@ import "./StateMachine.sol";
 /// @title A contract that implements the state machine pattern and adds time dependant transitions.
 contract TimedStateMachine is StateMachine {
 
-    event LogSetStateStartTime(bytes32 indexed _stateId, uint256 _startTime);
-    event LogChangeStateStartTime(bytes32 indexed _stateId, uint256 _startTime);
+    event LogSetTransitionTime(bytes32 indexed _startState, bytes32 indexed _endState, uint256 _timestamp);
+    event LogChangeTransitionTime(bytes32 indexed _startState, bytes32 indexed _endState, uint256 _timestamp);
 
     // Stores the start timestamp for each state (the value is 0 if the state doesn't have a start timestamp).
-    mapping(bytes32 => uint256) internal startTime;
+    mapping(bytes32 => mapping(bytes32 => uint256)) internal transitionTimes;
 
-    /// @dev Sets the starting timestamp for a state.
+    Transition timedTransition;
+    
+
+    /// @dev Sets the timestamp for a transition.
     /// @param _stateId The id of the state for which we want to set the start timestamp.
     /// @param _timestamp The start timestamp for the given state. It should be bigger than the current one.
-    function setStateStartTime(bytes32 _stateId, uint256 _timestamp) internal {
+    function createTimedTransition(bytes32 _startState, bytes32 _endState, uint256 _timestamp, function() internal _transitionEffect) internal {
         require(block.timestamp < _timestamp);
-        require(startTime[_stateId] == 0);
-        startTime[_stateId] = _timestamp;
-        addStartCondition(_stateId, hasStartTimePassed);
+        require(transitionTimes[_startState][_endState] == 0);
 
-        LogSetStateStartTime(_stateId, _timestamp);
+        transitionTimes[_startState][_endState] = _timestamp;
+        timedTransition = Transition(_startState, _endState, _transitionEffect, hasTransitionTimePassed);
+        createTransition(timedTransition);
+        LogSetTransitionTime(_startState, _endState, _timestamp);
     }
 
-    function changeStateStartTime(bytes32 _stateId, uint256 _timestamp) internal {
+    function updateTransitionTime(bytes32 _startState, bytes32 _endState, uint256 _timestamp) internal {
+        require(transitionTimes[_startState][_endState] != 0);
         require(block.timestamp < _timestamp);
-        require(startTime[_stateId] != 0);
-        startTime[_stateId] = _timestamp;
-
-        LogChangeStateStartTime(_stateId, _timestamp);
+        transitionTimes[_startState][_endState] = _timestamp;
 
     }
 
     /// @dev Returns the timestamp for the given state id.
     /// @param _stateId The id of the state for which we want to set the start timestamp.
-    function getStateStartTime(bytes32 _stateId) public view returns(uint256) {
-        return startTime[_stateId];
+    function getTransitionTime(bytes32 _startState, bytes32 _endState) public view returns(uint256) {
+        return transitionTimes[_startState][_endState];
     }
 
-    function hasStartTimePassed(bytes32 _stateId) internal returns(bool) {
-        return startTime[_stateId] <= block.timestamp;
+    function hasTransitionTimePassed(bytes32 _stateId) internal returns(bool) {
+        return transitionTimes[currentStateId][_stateId] <= block.timestamp;
     }
 
 }
