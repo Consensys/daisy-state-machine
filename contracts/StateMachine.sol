297cc55c93ad3a96a371fe2c5d4b75950362f301
vdrg <vrdragicevic@gmail.com>
2018-04-09 10:48:02 -0300

    Improve comments and add additional checks

--- contracts/StateMachine.sol
+++ contracts/StateMachine.sol
@@ -3,16 +3,20 @@ pragma solidity 0.4.19;
 
 contract StateMachine {
 
+    // Maps a stateId to the stateId of the next state
     mapping(bytes32 => bytes32) public nextStateId;
-    // The identifiers for the available functions in each state
+
+    // For each stateId, specify which functions are available
     mapping(bytes32 => mapping(bytes4 => bool)) public allowedFunctions;
 
-    mapping(bytes32 => function() internal[]) transitionCallbacks;
-    mapping(bytes32 => function(bytes32) internal returns(bool)[]) startConditions;
+    // Maps a stateId to an array of callbacks
+    mapping(bytes32 => function() internal[]) private transitionCallbacks;
+
+    // Maps a stateId to an array of conditions
+    mapping(bytes32 => function(bytes32) internal returns(bool)[]) private startConditions;
 
     // The current state id
     bytes32 public currentStateId;
-
 
     event LogTransition(bytes32 stateId, uint256 blockNumber);
 
@@ -25,15 +29,40 @@ contract StateMachine {
         _;
     }
 
-    /// @dev Creates the given states.
+    ///@dev transitions the state machine into the state it should currently be in
+    ///@dev by taking into account the current conditions and how many further transitions can occur 
+    function conditionalTransitions() public {
+
+        bytes32 next = nextStateId[currentStateId];
+
+        while (next != 0) {
+            // If one of the next state's conditions is met, go to this state and continue
+            bool stateChanged = false;
+            for (uint256 i = 0; i < startConditions[next].length; i++) {
+                if (startConditions[next][i](next)) {
+                    goToNextState();
+                    next = nextStateId[next];
+                    stateChanged = true;
+                    break;
+                }
+            }
+            // If none of the next state's conditions are met, then we are in the right current state
+            if (!stateChanged) break;
+        }
+    }
+
+    /// @dev Setup the state machine with the given states.
     /// @param stateIds Array of state ids.
     function setStates(bytes32[] stateIds) internal {
         require(stateIds.length > 0);
         require(currentStateId == 0);
 
+        require(stateIds[0] != 0);
+
         currentStateId = stateIds[0];
 
         for (uint256 i = 1; i < stateIds.length; i++) {
+            require(stateIds[i] != 0);
             nextStateId[stateIds[i - 1]] = stateIds[i];
         }
     }
@@ -71,27 +100,5 @@ contract StateMachine {
     ///@param callback The callback function to add (if the state is valid)
     function addCallback(bytes32 stateId, function() internal callback) internal {
         transitionCallbacks[stateId].push(callback);
-    }
-
-    ///@dev transitions the state machine into the state it should currently be in
-    ///@dev by taking into account the current conditions and how many further transitions can occur 
-    function conditionalTransitions() public {
-
-        bytes32 next = nextStateId[currentStateId];
-
-        while (next != 0) {
-            // If one of the next state's conditions is met, go to this state and continue
-            bool stateChanged = false;
-            for (uint256 i = 0; i < startConditions[next].length; i++) {
-                if (startConditions[next][i](next)) {
-                    goToNextState();
-                    next = nextStateId[next];
-                    stateChanged = true;
-                    break;
-                }
-            }
-            // If none of the next state's conditions are met, then we are in the right current state
-            if (!stateChanged) break;
-        }
     }
 }
